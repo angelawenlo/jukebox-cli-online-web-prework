5c45ca8a2bebd7c81b2db31c622bcd32d3aef9a0
Angela Lo <angelawenlo@gmail.com>
2019-05-28 19:27:48 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -34,7 +34,7 @@ def play(songs)
   if user_input == song
     puts "Playing #{song}"
   elsif user_input.to_i == i
-    puts "Playing #{songs[i-1]}"
+    puts "Playing #{songs[i]}"
   else
     puts "Invalid input, please try again"
    end
